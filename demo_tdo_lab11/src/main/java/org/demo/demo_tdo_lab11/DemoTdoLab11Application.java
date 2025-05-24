02e3265d357fa67844de3c84b715307b7521ca7b
stanoz <s.oziemczuk@gmail.com>
2025-05-24 15:42:05 +0200

    fixes

--- demo_tdo_lab11/src/main/java/org/demo/demo_tdo_lab11/DemoTdoLab11Application.java
+++ demo_tdo_lab11/src/main/java/org/demo/demo_tdo_lab11/DemoTdoLab11Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package org.demo.demo_tdo_lab11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoTdoLab11Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoTdoLab11Application.class, args);
     }
 
 }
